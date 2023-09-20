1f176cd74570c8b7d5c30e9dae70a50f8f953283
Robskan <100348563+Robskan5300@users.noreply.github.com>
2023-09-20 21:31:35 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Discord-Nexus', // Usually your GitHub org/user name.
+  projectName: 'Discord-Nexus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
